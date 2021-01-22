93637b2754f546e4db6f4cac88cfc10bb11a3bb2
Alex Vieira <alexvieiracb20@gmail.com>
2021-01-22 19:10:23 -0300

    Ajustado dois exercícios e concluído o 1004 e 1005

---
+++ uri-online-judge/src/main/resources/iniciante/Exercise1005.java
@@ -0,0 +1,22 @@
+package iniciante;
+
+import java.util.Scanner;
+
+/*
+* Calcular a média de dois produtos
+*/
+
+public class Exercise1005 {
+
+	 public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+	        
+        double a = leitor.nextDouble();
+        double b = leitor.nextDouble();
+        
+        double media = ((a * 3.5) / 11) + ((b * 7.5) / 11);
+        
+        System.out.printf("MEDIA = %.5f\n", media);
+	        
+	  }
+}
