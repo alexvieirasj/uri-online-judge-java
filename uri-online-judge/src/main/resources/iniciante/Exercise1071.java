14220d1f314c02d153d9fe973587fabcf9541c99
Alex Vieira <alexvieiracb20@gmail.com>
2021-03-02 21:39:05 -0300

    Exercício 1071 concluído

---
+++ uri-online-judge/src/main/resources/iniciante/Exercise1071.java
@@ -0,0 +1,35 @@
+package iniciante;
+
+import java.util.Scanner;
+
+/*
+* Mostrar a soma de números ímpares entre dois números
+*/
+
+public class Exercise1071 {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+
+		int X = leitor.nextInt();
+		int Y = leitor.nextInt();
+		int res = 0, i = 0, maior = 0, menor = 0;
+		
+		maior = X;
+		menor = Y;
+		
+		if(Y > maior) {
+			maior = Y;
+			menor = X;
+		}
+	
+		
+		for(i = menor+1; i < maior; i++ ) {
+			if(i % 2 != 0) {
+				res += i;
+			}
+		}
+		
+		System.out.println(res);
+	}
+}
