e4c62f1f3d701a10bd11d0713f9df2e39e7b24d7
Alex Vieira <alexvieiracb20@gmail.com>
2021-03-28 21:30:59 -0300

    Exercício 1133 concluído

---
+++ uri-online-judge/src/main/resources/iniciante/Exercise1133.java
@@ -0,0 +1,32 @@
+package iniciante;
+import java.util.Scanner;
+
+/* Mostrar divisão dos números por 5 for igual a 2 ou igual a 3 */
+
+public class Exercise1133 {
+
+	public static void main(String args[]) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int X = leitor.nextInt();
+		int Y = leitor.nextInt(); 
+		
+		int menor = X;
+		int maior = Y;
+		int soma = 0;
+		
+		if(Y < menor) {
+			menor = Y;
+			maior = X;
+		}
+		
+		for( int i = menor+1; i < maior; i++ ) {
+			
+			if(i % 5 == 2.0 || i % 5 == 3.0) {
+				System.out.println(i);
+			}
+		}
+		
+				
+	}
+}
