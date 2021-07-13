5318586b72c28f8924fd73a7def90a03ad344060
alexvieirasj <alexvieiracb20@gmail.com>
2021-07-13 00:05:25 -0300

    Exercício 2756

---
+++ uri-online-judge/src/main/resources/iniciante/Exercise2756.java
@@ -0,0 +1,26 @@
+package iniciante;
+
+/* Saída 10 */
+
+public class Exercise2756 {
+
+	public static void main(String args[]) {
+		
+		String a = "       A";
+		String b = "      B B";
+		String c = "     C   C";
+		String d = "    D     D";
+		String e = "   E       E";
+	
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(a);
+		
+	}
+}
